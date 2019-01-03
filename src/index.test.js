824f09ad871e8f173d50fb57c8689eedb338d35d
Joel T <dygerati@gmail.com>
2019-01-03 14:47:13 -0800

    Adding reminder template

--- src/index.test.js
+++ src/index.test.js
@@ -52,7 +52,6 @@ describe('Emails', () => {
    it('Offer email with no code with match snapshot', async () => {
       let newOffer = { ...orderMockData.offers[0] };
       delete newOffer.code;
-      const newOffers = [ ...orderMockData.offers ].concat([ newOffer ]);
 
       const compiled = await emails.compile('offer', {
          emailId: 'testEmailId',
@@ -64,6 +63,18 @@ describe('Emails', () => {
       });
 
       await fs.writeFile('./generated/offer_nocode.html', compiled.content);
+      expect(compiled.content).toMatchSnapshot();
+   });
+
+   it('Reminder email will match snapshot', async () => {
+      const compiled = await emails.compile('reminder', {
+         emailId: 'testEmailId',
+         cfunctions: 'http://notreally.com',
+         brand: brandMockData,
+         order: orderMockData
+      });
+
+      await fs.writeFile('./generated/reminder.html', compiled.content);
       expect(compiled.content).toMatchSnapshot();
    });
 });
--- src/types.js
+++ src/types.js
@@ -2,5 +2,9 @@ export default {
    offer: {
       filename: 'offer.mjml',
       subject: data => `Thanks ${data.order.customer.firstName} - here's your gift card`
+   },
+   reminder: {
+      filename: 'reminder.mjml',
+      subject: 'Reminder: eGift will expire soon'
    }
 };
